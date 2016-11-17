1a82a28a01187d9d48c27545bbacacaf74afd74f
Ramiro Alvarez <ramiroalvare2015@fau.edu>
2016-11-17 03:24:59 -0500

    Changed quite alot the look of the SnapCamp Site. Now it is more robust. Fixed the issue with adding anchors (link) tags in the description, That would generate a lot of extra read more tags

--- workspace/BootstrapStyle/BootstrapStyle/js/createBlogHome.js
+++ workspace/BootstrapStyle/BootstrapStyle/js/createBlogHome.js
@@ -1,11 +1,11 @@
 document.getElementsByClassName('plain')[0].setAttribute("id", "firstPlain");
-
 document.getElementsByClassName('panel-default')[0].style.border = "none";
 document.getElementsByClassName('panel-body')[0].style.border = "1px #ddd solid";
 document.getElementsByClassName('panel-title')[0].className += " blogTitle";
-$('#firstPlain .row a').each(function(){ $(this).addClass('btn btn-lg buttonw3c')})
 
-var readMoreAnchorTags = $('#firstPlain .row a');
+$('[id="ln8"]').each(function(){ $(this).addClass('btn btn-lg buttonw3c')})
+
+var readMoreAnchorTags = $('[id="ln8"]');
 
 for(var i = 0; i < readMoreAnchorTags.length; i++){
 
@@ -66,14 +66,14 @@ for(var i = 0; i < textParagraphTags.length; i++){
 var descriptionAnchors = $('.col-md-12').find('a#ln17');
 for(var i = 0; i < descriptionAnchors.length; i++){
 
-descriptionAnchors[i].className += ' btn-outline-primary';
+descriptionAnchors[i].setAttribute('class','col-xs-3 btn-outline-primary');
 }
 
-var descriptionAnchors = $('.col-md-12').find('a#ln18');
+var descriptionAnchors = $('.col-md-12').find('a#ln20');
 
 for(var i = 0; i < descriptionAnchors.length; i++){
 
-descriptionAnchors[i].className += ' btn-outline-info';
+descriptionAnchors[i].setAttribute('class','col-xs-4 btn btn-primary');
 }
 
 
@@ -85,3 +85,47 @@ for(var i = 0; i < timeElement .length; i++){
 
 }
 
+
+try{
+    document.getElementsByClassName('panel-title')[3].className += " blogTitle";
+
+    $('.PowerIndexUnit')[2].setAttribute('id','secondPlain');
+    $('#secondPlain').find('.string').each(function(){
+        $(this).addClass('myHeader');
+        $(this).attr('style','');
+    });
+
+    var readMoreAnchorTags2 = $('#secondPlain .row a');
+
+    for(var i = 0; i < readMoreAnchorTags2.length; i++){
+
+        readMoreAnchorTags2[i].innerHTML = '<span>READ MORE</span>'
+
+    }
+
+
+    $('[id="ln24"]').each(function(){ $(this).addClass('btn btn-lg buttonw3c')})
+
+    var readMoreAnchorTags = $('[id="ln24"]');
+
+    for(var i = 0; i < readMoreAnchorTags.length; i++){
+
+        readMoreAnchorTags[i].innerHTML = '<span>READ MORE</span>'
+
+    }
+
+    $('#resultTable').find('.string').each(function(){$(this).addClass('col-xs-5')});
+    var subTitle = " ";
+    var partOfTitle = $('#resultTable').find('.string');
+
+    for(var i = 0; i < partOfTitle.length; i++){
+        subTitle = partOfTitle[i].innerText;
+        subTitle = subTitle.replace(/(([^\s]+\s\s*){30})(.*)/,"$1…");
+        partOfTitle[i].innerText = subTitle + '...';
+
+    }
+
+}
+catch (err){
+    console.log(err);
+}
