d4618f1ca0a5d972c07950d3d22d255e6915cb33
Gabriel Delgado <gabsdel@gmail.com>
2016-11-26 11:02:51 -0500

    Delete Temp files directory

--- workspace/CEN5039/.Temp~/UpdateSQL.sql
+++
@@ -1,5 +0,0 @@
--- Comments_Comments [rel5]
-alter table `comments`  add column  `comments_oid`  integer;
-alter table `comments`   add index fk_comments_comments (`comments_oid`), add constraint fk_comments_comments foreign key (`comments_oid`) references `comments` (`oid`);
-
-
