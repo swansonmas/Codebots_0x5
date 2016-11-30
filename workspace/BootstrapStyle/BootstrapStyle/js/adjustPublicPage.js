fd17f7cfb725b45c1743f694b28ae0a1ad61f623
Ramiro Alvarez <ramiroalvare2015@fau.edu>
2016-11-30 14:04:16 -0500

    admin staff management

--- workspace/BootstrapStyle/BootstrapStyle/js/adjustPublicPage.js
+++ workspace/BootstrapStyle/BootstrapStyle/js/adjustPublicPage.js
@@ -14,16 +14,26 @@ document.getElementById('pretendReg').addEventListener('click', function(){
 	document.getElementById('fld5').value =  document.getElementById('pretendfl5').value;
 	document.getElementById('fld6').value =  document.getElementById('pretendfl6').value;
 
+
+	var fn = document.getElementById('pretendfl1').value;
+	var ln = document.getElementById('pretendfl2').value;
+	var email = document.getElementById('pretendfl3').value;
+	var usr = document.getElementById('pretendfl4').value;
+
 	var psw1 = document.getElementById("pretendfl5").value;
 	var psw2 = document.getElementById("pretendfl6").value;
 
 	if (psw1 != psw2) {
-		alert("Passwords Do Not Match");
+
 		document.getElementById("pretendfl5").style.borderColor = "#E34234";
 		document.getElementById("pretendfl6").style.borderColor = "#E34234";
+		$('#pwdChk').show();
+
 	}
+
 	else{
-		document.getElementById('button:ln9').click();
+
+		document.getElementById("button:ln9").click();
 	}
 
 
@@ -52,19 +62,26 @@ document.getElementById('pretenderLogin').addEventListener('click', function(){
 
 	if (loginUsr === '') {
 		document.getElementById("userNameValue").style.borderColor = "#E34234";
-		$('#loginFormUsr').show();
+		$('#checkUserName').show();
 	}
-	else if (loginPwd === '') {
+	else if(loginPwd === ''){
 		document.getElementById("passwordContent").style.borderColor = "#E34234";
-		$('#loginPwdVal').show();
+		$('#checkPwd').show();
+
+
 	}
 	else{
+
 		document.getElementById('button:ln2').click();
+
 	}
 
 
 
 });
+
+
+
 
 document.getElementById('userNameValue').addEventListener("keypress", function(event){
 	if(event.which === 13){
@@ -104,31 +121,14 @@ document.getElementById('cancelButton2').addEventListener('click', function(){
 
 
 
-	document.getElementById('pretendfl1').setAttribute('required', '');
-	document.getElementById('pretendfl2').setAttribute('required', '');
-	document.getElementById('pretendfl3').setAttribute('required', '');
-	document.getElementById('pretendfl4').setAttribute('required', '');
-	document.getElementById('pretendfl5').setAttribute('required', '');
-	document.getElementById('pretendfl6').setAttribute('required', '');
+document.getElementById('pretendfl1').setAttribute('required', '');
+document.getElementById('pretendfl2').setAttribute('required', '');
+document.getElementById('pretendfl3').setAttribute('required', '');
+document.getElementById('pretendfl4').setAttribute('required', '');
+document.getElementById('pretendfl5').setAttribute('required', '');
+document.getElementById('pretendfl6').setAttribute('required', '');
 
-	document.getElementById('pretendfl4').setAttribute('pattern', '^[\\w]{6,20}$');
-
-
-	document.getElementById('fld1').setAttribute('placeholder', 'example: John');
-	document.getElementById('fld2').setAttribute('placeholder', 'example: Doe');
-	document.getElementById('fld3').setAttribute('placeholder', 'example: JohnDoe@gmail.com');
-	document.getElementById('fld4').setAttribute('placeholder', 'username');
-	document.getElementById('fld5').setAttribute('placeholder', 'strong password');
-	document.getElementById('fld6').setAttribute('placeholder', 'repeat password');
-	document.getElementById('fld7').setAttribute('placeholder', 'type your username');
-	document.getElementById('fld8').setAttribute('placeholder', 'enter your password');
-
-var cancel = document.getElementById('cancelLoginForm');
-cancel.addEventListener('click', function(){
-
-	document.getElementById('loginForm_id').className= 'modalw3c';
-
-});
+document.getElementById('pretendfl4').setAttribute('pattern', '^[\\w]{6,20}$');
 
 document.getElementById('createAccount').addEventListener('click', function(){
 
@@ -148,3 +148,18 @@ document.getElementById('closeSpan').addEventListener('click', function(){
 
 	document.getElementById('regForm_id').className= 'modalw3c';
 });
+
+
+
+document.getElementById('closeSpan').addEventListener('click', function(){
+
+		document.getElementById('regForm_id').className= 'modalw3c';
+	});
+
+document.getElementById("cancelLoginForm").addEventListener('click', function(){
+
+		document.getElementById("loginForm_id").className= 'modalw3c';
+
+	});
+
+
---
+++ workspace/CEN5039/DBScripts/CEN5039_161123_152001.sql
@@ -0,0 +1,5 @@
+-- Post [ent1]
+alter table `post`  add column  `img_2`  varchar(255);
+alter table `post`  add column  `imgblob`  longblob;
+
+
